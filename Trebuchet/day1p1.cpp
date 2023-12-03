90d5a2a4af35497ef1b0ffeac3ab121f7b1f18ef
Mustafa Siddiqui <mustafa.a.r.siddiqui@outlook.com>
2023-12-03 17:42:02 -0500

    day1 p1 2023

---
+++ Trebuchet/day1p1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+auto main() -> int {
+    std::fstream fptr;
+    fptr.open("day1p1.txt", std::ios::in);
+    std::string token {};
+    int sum {};
+    while(std::getline(fptr, token)) {
+        int first = -1, last = -1;
+        for (char ch : token) {
+            if(isdigit(ch)) {
+                first < 0 ? first = (int)(ch) - 48 : 0;
+                last = (int)(ch) - 48;
+            }
+        }
+        // std::cout << first*10 + last << std::endl;
+        sum += first*10 + last;
+    }
+    std::cout << sum;
+}
