c85b07d8b17859fdc9a6070c3b288db241d85f73
Mustafa Siddiqui <mustafa.a.r.siddiqui@outlook.com>
2023-12-05 16:50:20 -0500

    day2-p2

---
+++ CubeConundrum/day2p2.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <algorithm>
+#include <unordered_map>
+
+class GameValues {
+private:
+    std::unordered_map<std::string, size_t> values;
+
+public:
+    GameValues() {
+        values["red"] = 0;
+        values["blue"] = 0;
+        values["green"] = 0;
+    }
+
+    size_t& operator[](const std::string& key) {
+        return values[key];
+    }
+
+    void reset() {
+        values["red"] = 0;
+        values["blue"] = 0;
+        values["green"] = 0;
+    }
+}game;
+
+auto main() -> int {
+    size_t sum {}, game_id {}, val {}, power {};
+    std::ifstream fptr;
+    fptr.open("day2p1.txt");
+    std::string token {}, str {};
+    
+    while(std::getline(fptr, token)) {
+        game.reset();
+        std::replace(token.begin(), token.end(), ',', ' ');
+        std::replace(token.begin(), token.end(), ';', ' ');
+        std::replace(token.begin(), token.end(), ':', ' ');
+
+        std::istringstream iss(token);
+        iss >> str >> game_id;
+
+        while (iss >> val >> str) 
+            game[str] = std::max(game[str], val);
+        
+
+        power = game["red"] * game["green"] * game["blue"];
+
+        sum += power;
+        
+    }
+    std::cout << sum;
+
+}
